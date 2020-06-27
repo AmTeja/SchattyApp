c63a7a742e6d0bccc129cd5d87e627751e7b8412
AmTeja <AmTeja@users.noreply.github.com>
2020-06-27 15:18:35 +0530

    Commit-4/06/2020 Look in changelog.

--- functions/index.js
+++ functions/index.js
@@ -3,4 +3,68 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-exports.sendNotification = fu
+exports.sendNotification = functions.region('asia-east2').firestore
+       .document('/ChatRoom/{ChatRoomId}/chats/{chatId}').onCreate((snap, context) => {
+           console.log('-----------FunctionStart-------------')
+
+           const doc = snap.data()
+           console.log(doc)
+
+           const idFrom = doc.sentFrom
+           const idTo = doc.sendTo
+           const contentMessage = doc.message
+           const imageUrl = doc.url
+
+           admin
+                .firestore()
+                .collection('users')
+                .where('uid', '==',idTo)
+                .get()
+                .then(querySnapshot => {
+                    querySnapshot.forEach(userTo => {
+                    const toUsername = userTo.data().username
+                        console.log('Found User to: ' + toUsername)
+                        if(userTo.data().token && userTo.data().sendTo !== idFrom){
+                            console.log('Token is: ' + userTo.data().token)
+                            admin
+                                .firestore()
+                                .collection('users')
+                                .where("uid", "==", idFrom)
+                                .get()
+                                .then(querySnapshot2 => {
+                                    querySnapshot2.forEach(userFrom => {
+                                        const fromUsername = userFrom.data().username
+                                        console.log('Found User from: ' + fromUsername)
+                                        const payload = {
+                                            notification: {
+                                                title: fromUsername,
+                                                body:  contentMessage,
+                                                badge: '1',
+                                                sound: 'default',
+                                                image: imageUrl,
+                                            },
+                                            data: {
+                                                click_action: "FLUTTER_NOTIFICATION_CLICK",
+                                                sentUser: fromUsername,
+                                                toUser: toUsername,
+                                            }
+                                        }
+
+                                        admin
+                                        .messaging()
+                                        .sendToDevice(userTo.data().token, payload)
+                                        .then(response => {
+                                            console.log('Successfully sent message: ',response)
+                                        })
+                                        .catch(error => {
+                                            console.log('Error sending message:', error)
+                                        })
+                                    })
+                                })
+                        } else{
+                            console.log('Cannot find token for target user')
+                        }
+                    })
+                })
+                return null
+       }) 
